2ed10714ca5105f1a483770c7d3ad4386f90c3ca
Mirage <mirageyangkang@outlook.com>
2017-12-06 09:09:43 +0800

    Linux 自动构建脚本

--- Build.sh
+++ Build.sh
@@ -1,7 +1,17 @@
+#!/bin/sh
+echo ****************************************  
+echo MirageEngine build begin  
+echo ****************************************  
+
 buildPath = "Build"
 
-if [ ! -d "$buildPath"];then
-    mkdir "$buildPath"
+if [ ! -d "${buildPath}" ]; then
+mkdir "${buildPath}"
 fi
-cd "$buildPath"
+cd "${buildPath}"
 cmake ../Src
+
+echo ****************************************  
+echo MirageEngine build over  
+echo ****************************************  
+pause  
